e997a405e4a9b8ad616513083734a1679e3de257
Erfan Hosseinpoor <erfanhoseinpor@gmail.com>
2022-12-08 18:10:50 +0330

    initial commit

---
+++ craco.config.ts
@@ -0,0 +1,24 @@
+import path from "path";
+import { CracoConfig } from "@craco/types/dist";
+
+const Config: CracoConfig = {
+    webpack: {
+        alias: {
+            "@components": path.resolve(__dirname, "src/components"),
+            "@constants": path.resolve(__dirname, "src/constants"),
+            "@containers": path.resolve(__dirname, "src/containers"),
+            "@contexts": path.resolve(__dirname, "src/contexts"),
+            "@layouts": path.resolve(__dirname, "src/layouts"),
+            "@pages": path.resolve(__dirname, "src/pages"),
+            "@routes": path.resolve(__dirname, "src/routes"),
+            "@services": path.resolve(__dirname, "src/services"),
+            "@styles": path.resolve(__dirname, "src/styles"),
+            "@utilities": path.resolve(__dirname, "src/utilities"),
+        },
+    },
+    devServer: {
+        port: 9001,
+    },
+};
+
+export default Config;
