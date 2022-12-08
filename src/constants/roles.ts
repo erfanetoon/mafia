d97343569e23b7ef1d84eeb3e4bdca4d157d607c
Erfan Hosseinpoor <erfanhoseinpor@gmail.com>
2022-12-08 18:13:00 +0330

    feat(styles): add all constants files

---
+++ src/constants/roles.ts
@@ -0,0 +1,12 @@
+const SS: IGameData<"rifleman"> = {
+    type: "rifleman",
+    users: [
+        {
+            id: 1,
+            name: "",
+            role: "s",
+        },
+    ],
+};
+
+export default SS;
